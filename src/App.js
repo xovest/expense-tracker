446f0390baccc1219fe917534fd142b5fdf58084
xovest <79008917+xovest@users.noreply.github.com>
2021-11-03 18:20:31 +0200

    ui added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { Balance } from './components/Balance';
+import { Header } from './components/Header';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
 
 function App() {
   return (
     <div>
-      Yo sup
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+      </div>
     </div>
   );
 }
