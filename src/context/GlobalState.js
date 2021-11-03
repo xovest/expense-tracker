8b69711c1a4e20f73f4ec72d881adb5cf6447983
xovest <79008917+xovest@users.noreply.github.com>
2021-11-03 20:14:58 +0200

    finished

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -3,12 +3,7 @@ import AppReducer from "./AppReducer";
 
 //we need an initial state
 const initialState = {
-  transactions: [
-    { id: 1, text: 'Flower', amount: -20 },
-    { id: 2, text: 'Salary', amount: 300 },
-    { id: 3, text: 'Book', amount: -10 },
-    { id: 4, text: 'Camera', amount: 150 }
-  ]
+  transactions: []
 };
 
 //creating the context
