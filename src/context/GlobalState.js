f4797f7d0151b3b558e1796fb1e0ed96406eaa81
xovest <79008917+xovest@users.noreply.github.com>
2021-11-03 18:42:22 +0200

    added the list of transactions

---
+++ src/context/GlobalState.js
@@ -0,0 +1,28 @@
+import { createContext, useReducer } from "react";
+import AppReducer from "./AppReducer";
+
+//we need an initial state
+const initialState = {
+  transactions: [
+    { id: 1, text: 'Flower', amount: -20 },
+    { id: 2, text: 'Salary', amount: 300 },
+    { id: 3, text: 'Book', amount: -10 },
+    { id: 4, text: 'Camera', amount: 150 }
+  ]
+};
+
+//creating the context
+export const GlobalContext = createContext(initialState);
+
+//provider components
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  return (
+    <GlobalContext.Provider value={{
+      transactions: state.transactions
+    }}>
+      {children}
+    </GlobalContext.Provider>
+  );
+};
